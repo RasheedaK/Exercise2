ae97aa26f381eaca8e5e53c66676366f94f05cec
krashee <krashee@thoughtworks.com>
2017-02-06 14:10:08 +0530

    Add more tests for equals method of Nucleotide class

--- test/NucleotideTest.java
+++ test/NucleotideTest.java
@@ -4,8 +4,13 @@ import static org.junit.Assert.*;
 
 public class NucleotideTest {
     @Test
-    public void shouldReturnTrueIfNucleotidesAreSame() {
+    public void shouldReturnEqualsIfNucleotidesAreSame() {
         Nucleotide nucleotide = new Nucleotide('C');
         assertEquals(new Nucleotide('C'), new Nucleotide('C'));
+    }
+    @Test
+    public void shouldReturnNotEqualsIfNucleotidesAreNotSame() {
+        Nucleotide nucleotide = new Nucleotide('C');
+        assertNotEquals(new Nucleotide('A'), new Nucleotide('C'));
     }
 }
