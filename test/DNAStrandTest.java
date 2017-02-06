fd44a9010a780c05be6bfc13211db59bd6a5ee99
krashee <krashee@thoughtworks.com>
2017-02-06 14:24:41 +0530

    Add a private method for hard coding nucleotides in DNAStrandTest

--- test/DNAStrandTest.java
+++ test/DNAStrandTest.java
@@ -8,24 +8,25 @@ import static org.junit.Assert.*;
 public class DNAStrandTest {
     @Test
     public void ShouldReturnThiamineIfIndex3() {
-        List<Nucleotide> nucleotides = new ArrayList<>();
-        nucleotides.add(new Nucleotide('C'));
-        nucleotides.add(new Nucleotide('G'));
-        nucleotides.add(new Nucleotide('A'));
-        nucleotides.add(new Nucleotide('T'));
+        List<Nucleotide> nucleotides = getNucleotides();
         DNAStrand dnaStrand = new DNAStrand(nucleotides);
         Nucleotide expectedNucleotide = new Nucleotide('T');
         assertEquals(expectedNucleotide, dnaStrand.getNucleotide(3));
     }
     @Test
     public void ShouldNotReturnThiamineIfIndex2() {
+        List<Nucleotide> nucleotides = getNucleotides();
+        DNAStrand dnaStrand = new DNAStrand(nucleotides);
+        Nucleotide expectedNucleotide = new Nucleotide('T');
+        assertNotEquals(expectedNucleotide, dnaStrand.getNucleotide(2));
+    }
+
+    private List<Nucleotide> getNucleotides() {
         List<Nucleotide> nucleotides = new ArrayList<>();
         nucleotides.add(new Nucleotide('C'));
         nucleotides.add(new Nucleotide('G'));
         nucleotides.add(new Nucleotide('A'));
         nucleotides.add(new Nucleotide('T'));
-        DNAStrand dnaStrand = new DNAStrand(nucleotides);
-        Nucleotide expectedNucleotide = new Nucleotide('T');
-        assertNotEquals(expectedNucleotide, dnaStrand.getNucleotide(2));
+        return nucleotides;
     }
 }
